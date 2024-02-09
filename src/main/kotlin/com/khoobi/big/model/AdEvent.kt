1c04121ed9d4d24e7b831362b0d9e21ee159b2dc
faeze khoobi <faezehkhoobi@yahoo.com>
2024-02-10 00:52:50 +0330

    fix data model

--- src/main/kotlin/com/khoobi/big/model/AdEvent.kt
+++ src/main/kotlin/com/khoobi/big/model/AdEvent.kt
@@ -7,19 +7,11 @@ import org.springframework.data.cassandra.core.mapping.Table
 @Table("ad_event")
 class AdEvent(
         @Id
-        @field:Column(("request_id"))
         val requestId: String,
-        @Column("ad_id")
         val adId: String,
-        @Column("ad_title")
         val adTitle: String,
-        @Column("advertiser_cost")
         val advertiserCost: Double,
-        @Column("app_id")
         val appId: String,
-        @Column("app_title")
         val appTitle: String,
-        @Column("impression_time")
         val impressionTime: Long,
-        @Column("click_time")
         var clickTime: Long)
